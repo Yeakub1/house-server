5d7739ba460251663e9a68fbeaf05ba524c49702
Yeakub1 <mryeakub10@gmail.com>
2024-01-24 00:36:52 +0600

    done project

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 // Use routes
 app.use("/auth", auth);
